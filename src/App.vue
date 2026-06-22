e65893990100d5c8a0e635b4c101f076f393314c
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-22 18:02:52 -0300

    style: Add pages transitions

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
 
 <style scoped></style>
