4e3ce7655a7e0bbd9a488e4efd2cba55fc68f5c3
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-22 18:00:37 -0300

    style: Add testing locks

---
+++ src/data/locks.ts
@@ -0,0 +1,18 @@
+import type { Lock } from '@/types/Lock'
+
+export const locks: Lock[] = [
+  {
+    slug: '40-cq',
+    name: '40 CQ',
+    image: '/locks/40cq.webp',
+    manualUrl: '/manuals/locks_digital_manual.pdf',
+    templateUrl: '/template/teste.txt',
+  },
+  {
+    slug: '22-se',
+    name: '22 SE',
+    image: '/locks/22SE.webp',
+    manualUrl: '/manuals/locks_digital_manual.pdf',
+    templateUrl: '/template/teste.txt',
+  },
+]
