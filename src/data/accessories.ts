12908a2cf0a4a4dabb1e675d12940834b4f50961
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-23 17:47:05 -0300

    test: Add accessories examples

---
+++ src/data/accessories.ts
@@ -0,0 +1,40 @@
+import type { Accessory } from '@/types/Accessory'
+
+export const accessories: Accessory[] = [
+  {
+    slug: 'fixador-de-portas',
+    name: 'Fixador de portas magnético',
+    image: '/accessories/fixador_magnetico.png',
+    manualUrl: '/manuals/accessories/teste_fixador.webp',
+  },
+  {
+    slug: 'fixador-de-portas',
+    name: 'Fixador de portas magnético',
+    image: '/accessories/fixador_magnetico.png',
+    manualUrl: '/manuals/accessories/teste_fixador.webp',
+  },
+  {
+    slug: 'fixador-de-portas',
+    name: 'Fixador de portas magnético',
+    image: '/accessories/fixador_magnetico.png',
+    manualUrl: '/manuals/accessories/teste_fixador.webp',
+  },
+  {
+    slug: 'fixador-de-portas',
+    name: 'Fixador de portas magnético',
+    image: '/accessories/fixador_magnetico.png',
+    manualUrl: '/manuals/accessories/teste_fixador.webp',
+  },
+  {
+    slug: 'fixador-de-portas',
+    name: 'Fixador de portas magnético',
+    image: '/accessories/fixador_magnetico.png',
+    manualUrl: '/manuals/accessories/teste_fixador.webp',
+  },
+  {
+    slug: 'fixador-de-portas',
+    name: 'Fixador de portas magnético',
+    image: '/accessories/fixador_magnetico.png',
+    manualUrl: '/manuals/accessories/teste_fixador.webp',
+  },
+]
